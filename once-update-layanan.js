4699b95febfc58c211d5554ea40806293ba47c84
cuanmaster <122278361+cuanmaster@users.noreply.github.com>
2025-06-12 15:58:51 +0700

    Update once-update-layanan.js
    decoded and fix some bugs

--- once-update-layanan.js
+++ once-update-layanan.js
@@ -1,1 +1,242 @@
-var _0x502f9b=_0x36f2;(function(_0x5b6e41,_0x5d798f){var _0x2c1611=_0x36f2,_0x5cca52=_0x5b6e41();while(!![]){try{var _0x188bca=parseInt(_0x2c1611(0x19e))/0x1*(parseInt(_0x2c1611(0x19f))/0x2)+parseInt(_0x2c1611(0x192))/0x3+-parseInt(_0x2c1611(0x198))/0x4+-parseInt(_0x2c1611(0x16e))/0x5+parseInt(_0x2c1611(0x180))/0x6*(-parseInt(_0x2c1611(0x1a3))/0x7)+parseInt(_0x2c1611(0x17f))/0x8*(parseInt(_0x2c1611(0x187))/0x9)+-parseInt(_0x2c1611(0x16c))/0xa*(parseInt(_0x2c1611(0x19d))/0xb);if(_0x188bca===_0x5d798f)break;else _0x5cca52['push'](_0x5cca52['shift']());}catch(_0xc50699){_0x5cca52['push'](_0x5cca52['shift']());}}}(_0x8a52,0x63b9e),console[_0x502f9b(0x176)]('\x0a\x09/**********************************************\x0a\x09\x20*\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20*\x0a\x09\x20*\x20\x20\x20\x20\x20\x20\x20\x20Created\x20By:\x20AMR\x20CODE\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20*\x0a\x09\x20*\x20\x20\x20\x20\x20\x20\x20\x20Visit:\x20https://fb.com/amir.ofc01\x20\x20\x20*\x0a\x09\x20*\x20\x20\x20\x20\x20\x20\x20\x20Tools\x20Gratis,\x20Tidak\x20Dijual\x20Belikan\x20*\x0a\x09\x20*\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20*\x0a\x09\x20**********************************************/\x0a\x09'));function _0x8a52(){var _0x4dd801=['2658WbqPDl','202oAkOth','.el-dialog__wrapper\x20.el-table__header-wrapper\x20thead\x20tr\x20th.el-table_2_column_16','i.sort-caret.ascending','.sc-table\x20.el-table__body-wrapper','7xGGsud','nextElementSibling','.el-dialog__wrapper\x20.el-table__body-wrapper\x20tbody\x20tr','.el-table_2_column_24\x20button.el-button--mini:not(.is-plain)','padStart','length','\x20Tidak\x20cocok\x20karna\x20layanan\x20bukan\x20multi\x20','ascending','is-checked','ERROR\x20SAVE','replace','match','\x20Tidak\x20cocok\x20karna\x20layanan\x20tidak\x20memiliki\x20faktur\x20pajak\x20','display','Timeout\x20waiting\x20for\x20deteksiTableSeller:\x20','10olsbHj','Data\x20tidak\x20ditemukan','2589105bmDOQx','.el-table_2_column_20','classList','\x20Cocok\x20dengan\x20rating\x20','\x20dan\x20harga\x20','value','.el-table__row','style','log','Tidak\x20ada\x20halaman\x20berikutnya','\x20Tidak\x20cocok\x20karna\x20rating\x20lebih\x20rendah\x20','forEach','split','innerText','\x20Result\x20:\x20','.el-button.el-button--primary','.el-table_2_column_16','22424dUOskb','1151034tWdplo','harga','substring','td.el-table_1_column_5','\x20Tidak\x20cocok\x20karna\x20layanan\x20memiliki\x20faktur\x20pajak\x20','Tidak\x20ada\x20yang\x20cocok,select\x20seller\x20close\x20','.el-dialog__wrapper','1845UFaiGp','click','.el-table_1_column_4\x20input','.el-button.el-button--primary.el-button--medium','.el-dialog__wrapper\x20+\x20div\x20.el-pager\x20.number.active','.el-dialog__wrapper\x20[aria-label=\x22Pilih\x20Seller\x22]\x20.el-dialog__headerbtn','.el-table_1_column_3\x20input','Ada\x20Error\x20exception,mungkin\x20seller\x20tidak\x20tersedia','.el-dialog__wrapper\x20.el-pager\x20li.number.active','dispatchEvent','.el-table_2_column_19','2185194IOgcnh','none','querySelector','input','.el-table_2_column_15','querySelectorAll','880096zLNbXO','hide','contains','show','.el-table_1_column_6\x20button','2566399mUthHP'];_0x8a52=function(){return _0x4dd801;};return _0x8a52();}function wait(_0x1af391){return new Promise(_0x79f8f8=>setTimeout(_0x79f8f8,_0x1af391));}async function deteksiTableSeller(_0x30bff4=_0x502f9b(0x19b),_0x5b6464=0xea60){return new Promise((_0x4ed274,_0x8eeefe)=>{const _0x2d9a2c=setInterval(()=>{var _0x153461=_0x36f2;const _0x5ccc07=document[_0x153461(0x194)](_0x153461(0x186));if(_0x5ccc07){let _0x3ba2d5=_0x5ccc07[_0x153461(0x175)][_0x153461(0x16a)];if(_0x30bff4==_0x153461(0x19b)&&_0x3ba2d5!='none')clearInterval(_0x2d9a2c),clearTimeout(_0x42e50d),_0x4ed274(_0x5ccc07);else _0x30bff4==_0x153461(0x199)&&_0x3ba2d5==_0x153461(0x193)&&(clearInterval(_0x2d9a2c),clearTimeout(_0x42e50d),_0x4ed274(_0x5ccc07));}},0x64),_0x42e50d=setTimeout(()=>{var _0x2c979d=_0x36f2;clearInterval(_0x2d9a2c),_0x4ed274(console[_0x2c979d(0x176)](_0x2c979d(0x16b)+element));},_0x5b6464);});}async function deteksiSaveModal(_0x1d82d6='show',_0x12b1d1=0xea60){return new Promise((_0x24232a,_0x4bf1d9)=>{const _0x3ad296=setInterval(()=>{var _0x1a7fb8=_0x36f2,_0x612499=document['querySelectorAll'](_0x1a7fb8(0x186)),_0x58b474=_0x612499[0x2];if(_0x58b474){let _0x317936=_0x58b474['style']['display'];if(_0x1d82d6==_0x1a7fb8(0x19b)&&_0x317936!=_0x1a7fb8(0x193))clearInterval(_0x3ad296),clearTimeout(_0x3e7d3c),_0x24232a(_0x58b474);else _0x1d82d6=='hide'&&_0x317936==_0x1a7fb8(0x193)&&(clearInterval(_0x3ad296),clearTimeout(_0x3e7d3c),_0x24232a(_0x58b474));}},0x64),_0x3e7d3c=setTimeout(()=>{var _0x1d030d=_0x36f2;clearInterval(_0x3ad296),_0x24232a(console[_0x1d030d(0x176)](_0x1d030d(0x16b)+element));},_0x12b1d1);});}function generateCode(_0x3ad59a,_0x484b1d){var _0x5b890e=_0x502f9b;const _0xe269fa=_0x3ad59a[_0x5b890e(0x17a)]('\x20');let _0x242d26='';return _0xe269fa[_0x5b890e(0x179)](_0x418a3a=>{_0x242d26+=_0x418a3a[0x0];}),_0x242d26+=_0x484b1d['toString']()[_0x5b890e(0x1a7)](0x2,'0'),_0x242d26[_0x5b890e(0x182)](0x0,0xa);}async function selectSeller(_0x5b7e95=![],_0x41db5c=![],_0x4fd0d9=![]){var _0x1573fb=_0x502f9b,_0xfa913c=document[_0x1573fb(0x194)](_0x1573fb(0x1a0));if(!_0xfa913c[_0x1573fb(0x170)]['contains'](_0x1573fb(0x1aa))){var _0x3b582d=_0xfa913c[_0x1573fb(0x194)](_0x1573fb(0x1a1));_0x3b582d[_0x1573fb(0x188)](),await wait(0x3e8);}var _0x26eeed=document[_0x1573fb(0x197)](_0x1573fb(0x1a5)),_0x3b570e={},_0x5d006e=![];for(var _0x32ae96 of _0x26eeed){_0x5d006e=!![];var _0x1344ad=_0x32ae96[_0x1573fb(0x194)]('.el-table_2_column_14\x20.el-row:nth-child(1)')['innerText'],_0x19c393=_0x32ae96[_0x1573fb(0x194)](_0x1573fb(0x196))['innerText'],_0xd82617=_0x32ae96[_0x1573fb(0x194)](_0x1573fb(0x191))['innerText'],_0x427ef4=_0x32ae96[_0x1573fb(0x194)](_0x1573fb(0x16f))[_0x1573fb(0x17b)],_0x1975ea=_0x32ae96[_0x1573fb(0x194)](_0x1573fb(0x17e))['innerText'];try{_0x19c393=_0x19c393[_0x1573fb(0x168)](/([0-9.]+) \(/)[0x1];}catch(_0xb776dd){_0x19c393=0x0;}_0x427ef4=_0x427ef4['match'](/Ya/i)?!![]:![],_0xd82617=_0xd82617[_0x1573fb(0x168)](/Ya/i)?!![]:![];var _0x3e0c8e=_0x1975ea;_0x1975ea=_0x1975ea['match'](/[0-9.]+/)[0x0],_0x1975ea=_0x1975ea[_0x1573fb(0x167)]('.','');_0x5b7e95!==![]&&(parseFloat(_0x19c393)<parseFloat(_0x5b7e95)&&(console[_0x1573fb(0x176)](_0x1344ad+_0x1573fb(0x178)+_0x19c393),_0x5d006e=![]));_0x41db5c==!![]&&(_0x427ef4==![]&&(console['log'](_0x1344ad+_0x1573fb(0x1a9)),_0x5d006e=![]));_0x4fd0d9==!![]?_0xd82617==![]&&(console[_0x1573fb(0x176)](_0x1344ad+_0x1573fb(0x169)),_0x5d006e=![]):_0xd82617==!![]&&(console[_0x1573fb(0x176)](_0x1344ad+_0x1573fb(0x184)),_0x5d006e=![]);if(_0x5d006e==!![]){var _0x5d18bd=_0x32ae96[_0x1573fb(0x194)](_0x1573fb(0x1a6))['click']();console[_0x1573fb(0x176)](_0x1344ad+_0x1573fb(0x171)+_0x19c393+_0x1573fb(0x172)+_0x3e0c8e),await deteksiTableSeller(_0x1573fb(0x199)),_0x3b570e={'nama':_0x1344ad,'rating':_0x19c393,'harga':_0x3e0c8e};break;}}if(_0x5d006e==![]){var _0x71f44a=document[_0x1573fb(0x194)](_0x1573fb(0x18f)),_0x4d64da=_0x71f44a?_0x71f44a['nextElementSibling']:undefined;return _0x4d64da?(_0x4d64da[_0x1573fb(0x188)](),await wait(0x1f4),await selectSeller(_0x5b7e95,_0x41db5c,_0x4fd0d9)):(console['log']('Tidak\x20ada\x20yang\x20cocok,select\x20seller\x20close\x20'),document['querySelector'](_0x1573fb(0x18c))[_0x1573fb(0x188)](),await deteksiTableSeller(_0x1573fb(0x199)),console[_0x1573fb(0x176)]('Tidak\x20ada\x20halaman\x20berikutnya'),null);}return await wait(0x1f4),_0x3b570e;}async function triggerSave(){var _0x24dad0=_0x502f9b;try{var _0xdd760d=document['querySelector'](_0x24dad0(0x18a));_0xdd760d[_0x24dad0(0x188)](),await deteksiSaveModal(_0x24dad0(0x19b),0x2710);var _0x4dc0e9=document[_0x24dad0(0x197)](_0x24dad0(0x186)),_0x503a2c=_0x4dc0e9[0x2],_0x224653=_0x503a2c[_0x24dad0(0x194)](_0x24dad0(0x17d));_0x224653[_0x24dad0(0x188)](),await deteksiSaveModal(_0x24dad0(0x199),0x2710);}catch(_0x43ad0e){console[_0x24dad0(0x176)](_0x24dad0(0x166));}}function _0x36f2(_0x157eac,_0x34c47b){var _0x8a527a=_0x8a52();return _0x36f2=function(_0x36f242,_0x393942){_0x36f242=_0x36f242-0x165;var _0x251dec=_0x8a527a[_0x36f242];return _0x251dec;},_0x36f2(_0x157eac,_0x34c47b);}async function start(_0x4da707=![]){var _0x20cb08=_0x502f9b,_0x24ff51=document[_0x20cb08(0x197)](_0x20cb08(0x1a2));if(_0x24ff51[_0x20cb08(0x1a8)]==0x0)return console['log'](_0x20cb08(0x16d)),![];else _0x24ff51[_0x20cb08(0x1a8)]==0x2?_0x24ff51=_0x24ff51[0x1][_0x20cb08(0x197)]('.el-table__row'):_0x24ff51=_0x24ff51[0x0][_0x20cb08(0x197)](_0x20cb08(0x174));no=0x1;try{for(let _0x5b4478 of _0x24ff51){var _0x57a30f=_0x5b4478[_0x20cb08(0x194)](_0x20cb08(0x183))[_0x20cb08(0x17b)],_0x135cf8=_0x57a30f[_0x20cb08(0x168)](/([0-9]+).([0-9]+)?.([0-9]+)?/),_0x41de5d=generateCode(_0x57a30f,no),_0x429b54=_0x5b4478[_0x20cb08(0x194)](_0x20cb08(0x18d)),_0x931a90=_0x5b4478[_0x20cb08(0x194)](_0x20cb08(0x189));_0x429b54?.[_0x20cb08(0x173)]?.[_0x20cb08(0x1a8)]>0x1&&(_0x41de5d=_0x429b54['value']);_0x429b54[_0x20cb08(0x173)]=_0x41de5d,_0x429b54[_0x20cb08(0x190)](new Event('input',{'bubbles':!![]}));var _0x142a97=_0x5b4478[_0x20cb08(0x194)](_0x20cb08(0x19c))[_0x20cb08(0x188)]();await deteksiTableSeller('show');var _0x4878ca=await selectSeller(_0x4da707);if(!_0x4878ca){console[_0x20cb08(0x176)]('Gagal\x20Memilih\x20Seller\x20untuk\x20layanan\x20'+_0x57a30f);continue;}console[_0x20cb08(0x176)](_0x57a30f,_0x20cb08(0x17c)+_0x4878ca);try{let _0x503ff2=_0x4878ca?.[_0x20cb08(0x181)]['replace'](/\D/g,'');_0x931a90[_0x20cb08(0x173)]=_0x503ff2,_0x931a90['dispatchEvent'](new Event('input',{'bubbles':!![]}));}catch(_0xe5157c){console[_0x20cb08(0x176)](_0xe5157c);}var _0x337d73=_0x5b4478[_0x20cb08(0x194)]('.el-table_1_column_11\x20.el-switch');!_0x337d73['classList'][_0x20cb08(0x19a)](_0x20cb08(0x165))&&(_0x337d73[_0x20cb08(0x194)](_0x20cb08(0x195))['click'](),await wait(0x2bc)),no++,await wait(0xc8);}}catch(_0x30d7f0){console[_0x20cb08(0x176)](_0x20cb08(0x18e),_0x30d7f0);}await triggerSave();try{var _0x4c4be9=document[_0x20cb08(0x194)](_0x20cb08(0x18b)),_0x5eb227=_0x4c4be9?_0x4c4be9[_0x20cb08(0x1a4)]:undefined;_0x5eb227?(_0x5eb227[_0x20cb08(0x188)](),await wait(0x1f4),start(_0x4da707)):(console[_0x20cb08(0x176)](_0x20cb08(0x185)),document[_0x20cb08(0x194)](_0x20cb08(0x18c))['click'](),await deteksiTableSeller(_0x20cb08(0x199)),console[_0x20cb08(0x176)](_0x20cb08(0x177)));}catch(_0x2097ba){}}start();
+console.log(`
+\t/**********************************************
+\t *                                            *
+\t *        Created By: AMR CODE               *
+\t *        Modified By: Unicode               *
+\t *        Visit: https://fb.com/amir.ofc01   *
+\t *        Tools Gratis, Tidak Dijual Belikan *
+\t *                                            *
+\t **********************************************/
+`);
+
+function wait(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function waitForSellerTable(state = 'show', timeout = 60000) {
+  return new Promise((resolve) => {
+    const interval = setInterval(() => {
+      const wrapper = document.querySelector('.el-dialog__wrapper');
+      if (wrapper) {
+        let display = wrapper.style.display;
+        if ((state === 'show' && display !== 'none') || (state === 'hide' && display === 'none')) {
+          clearInterval(interval);
+          clearTimeout(timer);
+          resolve(wrapper);
+        }
+      }
+    }, 100);
+    const timer = setTimeout(() => {
+      clearInterval(interval);
+      resolve(console.log('Timeout waiting for deteksiTableSeller: ' + state));
+    }, timeout);
+  });
+}
+
+async function waitForSaveModal(state = 'show', timeout = 60000) {
+  return new Promise((resolve) => {
+    const interval = setInterval(() => {
+      const wrappers = document.querySelectorAll('.el-dialog__wrapper');
+      const modal = wrappers[2];
+      if (modal) {
+        let display = modal.style.display;
+        if ((state === 'show' && display !== 'none') || (state === 'hide' && display === 'none')) {
+          clearInterval(interval);
+          clearTimeout(timer);
+          resolve(modal);
+        }
+      }
+    }, 100);
+    const timer = setTimeout(() => {
+      clearInterval(interval);
+      resolve(console.log('Timeout waiting for deteksiSaveModal: ' + state));
+    }, timeout);
+  });
+}
+
+function generateServiceCode(serviceName, number, maxCodeLength = 15) {
+  // Bersihkan nama service dari karakter khusus dan ubah ke uppercase
+  const cleanedName = serviceName.replace(/[^a-zA-Z0-9\s]/g, '').toUpperCase();
+  
+  // Pisahkan menjadi kata-kata dan ambil huruf dari setiap kata
+  const words = cleanedName.split(/\s+/).filter(word => word.length > 0);
+  let code = '';
+  
+  // Ambil lebih banyak huruf dari setiap kata (maksimal 3 huruf per kata)
+  words.forEach(word => {
+    code += word.substring(0, 3); // Ambil 3 huruf pertama dari setiap kata
+  });
+  
+  // Tambahkan nomor dengan padding 2 digit
+  const numericPart = number.toString().padStart(2, '0');
+  code += numericPart;
+  
+  // Hanya ambil karakter alfanumerik dan batasi panjang maksimal
+  const alphanumericCode = code.replace(/[^A-Z0-9]/g, '');
+  return alphanumericCode.substring(0, maxCodeLength);
+}
+
+async function chooseSeller(minRating = 3.5, requireMulti = false, requireInvoice = false) {
+  const sortColumn = document.querySelector('.el-dialog__wrapper .el-table__header-wrapper thead tr th.el-table_2_column_16');
+  if (!sortColumn.classList.contains('ascending')) {
+    sortColumn.querySelector('i.sort-caret.ascending').click();
+    await wait(1000);
+  }
+
+  const sellerRows = document.querySelectorAll('.el-dialog__wrapper .el-table__body-wrapper tbody tr');
+  let selectedSeller = {};
+  let foundValid = false;
+
+  for (const row of sellerRows) {
+    foundValid = true;
+    const sellerName = row.querySelector('.el-table_2_column_14 .el-row:nth-child(1)').innerText;
+    let rating = row.querySelector('.el-table_2_column_15').innerText;
+    const invoice = row.querySelector('.el-table_2_column_19').innerText.match(/Ya/i) ? true : false;
+    const multiService = row.querySelector('.el-table_2_column_20').innerText.match(/Ya/i) ? true : false;
+    const priceText = row.querySelector('.el-table_2_column_16').innerText;
+
+    try {
+      rating = parseFloat(rating.match(/([0-9.]+) \(/)[1]);
+    } catch {
+      rating = 0;
+    }
+
+    const priceClean = priceText.match(/[0-9.]+/)[0].replace('.', '');
+
+    if (rating < minRating) {
+      console.log(`${sellerName} Tidak cocok karena rating ${rating} lebih rendah dari minimum ${minRating}`);
+      foundValid = false;
+    }
+
+    if (requireMulti && !multiService) {
+      console.log(`${sellerName} Tidak cocok karena layanan bukan multi`);
+      foundValid = false;
+    }
+
+    if (requireInvoice && !invoice) {
+      console.log(`${sellerName} Tidak cocok karena layanan tidak memiliki faktur pajak`);
+      foundValid = false;
+    } else if (!requireInvoice && invoice) {
+      console.log(`${sellerName} Tidak cocok karena layanan memiliki faktur pajak`);
+      foundValid = false;
+    }
+
+    if (foundValid) {
+      row.querySelector('.el-table_2_column_24 button.el-button--mini:not(.is-plain)').click();
+      console.log(`${sellerName} Cocok dengan rating ${rating} dan harga ${priceText}`);
+      await waitForSellerTable('hide');
+      selectedSeller = { nama: sellerName, rating: rating, harga: priceText };
+      break;
+    }
+  }
+
+  if (!foundValid) {
+    const currentPage = document.querySelector('.el-dialog__wrapper .el-pager li.number.active');
+    const nextPage = currentPage?.nextElementSibling;
+    if (nextPage) {
+      nextPage.click();
+      await wait(500);
+      return await chooseSeller(minRating, requireMulti, requireInvoice);
+    } else {
+      console.log('Tidak ada seller yang cocok, menutup dialog pemilihan');
+      document.querySelector('.el-dialog__wrapper [aria-label="Pilih Seller"] .el-dialog__headerbtn').click();
+      await waitForSellerTable('hide');
+      return null;
+    }
+  }
+
+  await wait(500);
+  return selectedSeller;
+}
+
+async function triggerSave() {
+  try {
+    const primaryBtn = document.querySelector('.el-button.el-button--primary.el-button--medium');
+    primaryBtn.click();
+    await waitForSaveModal('show', 10000);
+
+    const modals = document.querySelectorAll('.el-dialog__wrapper');
+    const modal = modals[2];
+    const saveBtn = modal.querySelector('.el-button.el-button--primary');
+    saveBtn.click();
+
+    await waitForSaveModal('hide', 10000);
+  } catch {
+    console.log('ERROR SAVE');
+  }
+}
+
+async function processAllServices(checkRating = true) {
+  let tables = document.querySelectorAll('.sc-table .el-table__body-wrapper');
+  if (tables.length === 0) {
+    console.log('Data tidak ditemukan');
+    return false;
+  }
+
+  let serviceRows = tables.length === 2
+    ? tables[1].querySelectorAll('.el-table__row')
+    : tables[0].querySelectorAll('.el-table__row');
+
+  let no = 1;
+
+  try {
+    for (const row of serviceRows) {
+      const serviceName = row.querySelector('td.el-table_1_column_5').innerText;
+      const code = generateServiceCode(serviceName, no);
+      const inputName = row.querySelector('.el-table_1_column_3 input');
+      const inputPrice = row.querySelector('.el-table_1_column_4 input');
+
+      if (inputName?.value?.length > 1) {
+        inputName.value = inputName.value;
+      } else {
+        inputName.value = code;
+      }
+
+      inputName.dispatchEvent(new Event('input', { bubbles: true }));
+      row.querySelector('.el-table_1_column_6 button').click();
+
+      await waitForSellerTable('show');
+      const selected = await chooseSeller(3.5); // Minimal rating 3.5
+
+      if (!selected) {
+        console.log('Gagal Memilih Seller untuk layanan ' + serviceName);
+        continue;
+      }
+
+      console.log(serviceName, ' Result : ', selected);
+
+      inputPrice.value = '0';
+      inputPrice.dispatchEvent(new Event('input', { bubbles: true }));
+
+      const switchToggle = row.querySelector('.el-table_1_column_11 .el-switch');
+      if (!switchToggle.classList.contains('is-checked')) {
+        switchToggle.querySelector('input').click();
+        await wait(700);
+      }
+
+      no++;
+      await wait(200);
+    }
+  } catch (err) {
+    console.log('Ada Error exception, mungkin seller tidak tersedia', err);
+  }
+
+  await triggerSave();
+
+  try {
+    const activePage = document.querySelector('.el-dialog__wrapper + div .el-pager .number.active');
+    const nextPage = activePage?.nextElementSibling;
+    if (nextPage) {
+      nextPage.click();
+      await wait(500);
+      processAllServices(checkRating);
+    } else {
+      console.log('Tidak ada yang cocok,select seller close');
+      document.querySelector('.el-dialog__wrapper [aria-label="Pilih Seller"] .el-dialog__headerbtn').click();
+      await waitForSellerTable('hide');
+      console.log('Tidak ada halaman berikutnya');
+    }
+  } catch {}
+}
+
+processAllServices();
